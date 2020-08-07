4d18fceb71ca78c21b56babb7018eb07162989e2
R M Lakruwan <lakathekolla@gmail.com>
2020-08-07 20:17:18 +0530

    Update PrintPlot.py

--- Bluetooth Helping/PrintPlot.py
+++ Bluetooth Helping/PrintPlot.py
@@ -1,3 +1,11 @@
+'''
+Title : Bluetooth Helping Text to plot
+Author : R M Lakruwan @ Noone
+Date : 07 Aug 2020
+Compatibality : Python 3
+version : 1.0
+'''
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
@@ -6,8 +14,11 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
 def animate(i):
+    # get data from text file to python array
     pullData = open("a.txt","r").read()
     dataArray = pullData.split('\n')
+    
+    # for cordinate
     xar = []
     yar = []
     for eachLine in dataArray:
@@ -17,5 +28,7 @@ def animate(i):
             yar.append(int(y))
     ax1.clear()
     ax1.plot(xar,yar)
+
+# Time plot refreshing data under interval
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
